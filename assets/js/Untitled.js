c06523b2a6ea992e0fd71e85c0a5d7fc728e852f
mohammedbabiker <mobabikerbabai20@gmail.com>
2023-01-15 06:39:34 -0800

    Add files via upload

---
+++ assets/js/Untitled.js
@@ -0,0 +1,7 @@
+<!DocumentType JavaScript>
+    <JavaScript lang="en">
+        <meta charset="UTF-8">
+        <meta lang="en">
+        
+
+    </JavaScript>
---
+++ assets/js/linkAction.js
@@ -0,0 +1,5 @@
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu');
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu');
+}
